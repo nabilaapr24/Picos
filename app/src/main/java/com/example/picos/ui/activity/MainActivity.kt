cbd6a3b3c9bce9ed005c79d652e437012ce43feb
hardhikapro <propitadewa@gmail.com>
2023-06-11 19:09:36 +0700

    fixed error, tampilan home done

--- app/src/main/java/com/example/picos/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/picos/ui/activity/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.picos.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.Intent
 import android.os.Bundle
-import com.example.picos.R
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.picos.databinding.ActivityMainDashboardBinding
+import com.example.picos.ui.fragment.HomeFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.picos.R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+
+        val fragment = HomeFragment()
+        fragmentTransaction.replace(com.example.picos.R.id.fragment_container_view_tag, fragment)
+        fragmentTransaction.commit()
+
     }
+
 }
